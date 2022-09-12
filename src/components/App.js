76665190a10330d21b9aecb7c5325e225c271744
Kristian <kris.dziamka@gmail.com>
2022-09-12 22:52:25 +0300

    task3

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,24 @@
-import Profile from './Profile/Profile'
-import Statistics from './Statistics/Statistics'
-import user from './data/user.json'
-import data from './data/data.json'
+import Profile from './Profile/Profile';
+import Statistics from './Statistics/Statistics';
+import FriendList from './Friends/FriendList';
+import user from './data/user.json';
+import data from './data/data.json';
+import friends from './data/friends.json';
 
-
-
-
- export const App = () => {
+export const App = () => {
   return (
-<>
-  <Profile username={user.username} tag={user.tag} location={user.location} avatar={user.avatar} stats={user.stats}/>
-  <Statistics title="Upload stats" stats={data}/>
-  <Statistics stats={data} />;
-  </>
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
+      <FriendList friends={friends} />
+ 
+    </>
   );
 };
